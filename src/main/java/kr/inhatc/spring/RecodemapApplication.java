35719a40ba03328448807910a557b7cbdfbe913b
rueeeeeeng <qhaud7023@naver.com>
2022-12-23 02:29:12 +0900

    깃 생성

---
+++ src/main/java/kr/inhatc/spring/RecodemapApplication.java
@@ -0,0 +1,13 @@
+package kr.inhatc.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecodemapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecodemapApplication.class, args);
+	}
+
+}
